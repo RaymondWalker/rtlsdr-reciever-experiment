f43bfc3893b375e5762b8adfc1f48f2909739a59
Raymond Walker <raymond.j.walker@protonmail.com>
2026-06-09 10:41:22 -0400

    Sample Reading Test

---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <rtl-sdr.h>
+
+int main() 
+{ 
+    std:: cout << "SDR Capture Test" << std::endl;
+
+    // check for available device
+    uint32_t device_count = rtlsdr_get_device_count();
+    if (device_count == 0 ) {
+        std::cerr << "ERROR : No devices found :( " << std::endl;
+        return 1;
+    }
+
+    std::cout << "Found " << device_count << "Supported devices" << std::endl;
+    std::cout << "Device name: " << rtlsdr_get_device_name(0) << std::endl;
+
+    // Open the device
+    
+    //safely create dev variable
+    rtlsdr_dev_t *dev = nullptr;
+    if (rtlsdr_open(&dev,0) < 0) {
+        std::cerr << "ERROR: Failed to open device." << std::endl;
+        return 1;
+    }
+    std::cerr << "Device Opened!" << std::endl;
+
+    // Configure Device
+    uint32_t target_freq = 107100000; //107.1 MHz
+    int sample_rate = 2048000; //2.048 Mega-samples per second
+
+    if (rtlsdr_set_sample_rate(dev, sample_rate) < 0)
+    {
+        std::cerr << "ERROR: Failed to set sample rate" << std::endl;
+
+    }else{
+        std::cout << "Sample Rate set to: " << sample_rate << " Hz" << std::endl;
+    }
+
+    if (rtlsdr_set_center_freq(dev, target_freq) < 0) {
+        std::cerr << "Failed to set center frequency" << std::endl;
+
+    } else {
+        std::cout << "Successfully tuned to " << target_freq / 1000000.0 << "MHz" << std::endl;
+
+    }
+
+
+    //clear old buffer in case
+    rtlsdr_reset_buffer(dev);
+    
+    //allocate buffer
+    int bytes_to_read = 16*1024;
+    uint8_t *buffer = (uint8_t *)malloc(bytes_to_read);
+    int n_read = 0;
+
+    // Read samples
+    if (rtlsdr_read_sync(dev, buffer, bytes_to_read, &n_read) >= 0)
+    {
+        std::cout << "Read " << n_read << std::endl;
+    }
+
+
+    // cleanup
+    free(buffer);
+    rtlsdr_close(dev);
+
+    return 0;
+}
